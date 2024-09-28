e7c65643f12205a12d85e239be4eae018aeacc63
sadek1999 <sadekali1530@gmail.com>
2024-09-28 21:52:23 +0600

    set router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+
 import './index.css'
+import { RouterProvider } from 'react-router-dom'
+import { router } from './routes/routes.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+ <RouterProvider router={router}></RouterProvider>
   </React.StrictMode>,
 )
