d4f4a3af8fd18e1983285949c6c89e44ef5e3a71
sadek1999 <sadekali1530@gmail.com>
2024-10-28 18:52:04 +0600

    set enponst for get all sutdents

--- src/redux/features/admin/userManagement.ts
+++ src/redux/features/admin/userManagement.ts
@@ -1,9 +1,30 @@
+import { TQueryParams, TResponseRedux } from "../../../Types";
 import { baseApi } from "../../api/baseApi";
 
-const userManagementApi=baseApi.injectEndpoints({
-    endpoints:(build)=>({
+const userManagementApi = baseApi.injectEndpoints({
+  endpoints: (builder) => ({
+    getStudent: builder.query({
+      query: (args) => {
+        const params = new URLSearchParams();
+        if (args) {
+          args.forEach((item: TQueryParams) => {
+            params.append(item.name, item.value as string);
+          });
+        }
+        return {
+          url: "",
+          method: "GET",
+          params: params,
+        };
+      },
+      transformResponse: (response: TResponseRedux<any>) => {
+        return {
+          data: response.data,
+          meta: response.meta,
+        };
+      },
+    }),
+  }),
+});
 
-    })
-})
-
-export const {}=userManagementApi;
+export const {} = userManagementApi;
