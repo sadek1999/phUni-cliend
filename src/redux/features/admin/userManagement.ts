ea6906f5f08dae5ada247cd22d1b6684dbaa773b
sadek1999 <sadekali1530@gmail.com>
2024-11-01 23:10:33 +0600

    new

--- src/redux/features/admin/userManagement.ts
+++ src/redux/features/admin/userManagement.ts
@@ -61,6 +61,20 @@ const userManagementApi = baseApi.injectEndpoints({
         body:data
       })
     })
+    addFaculty:builder.mutation({
+      query:(data)=>({
+        url:"users/create-faculty",
+        method:"POST",
+        body:data
+      })
+    })
+    addFaculty:builder.mutation({
+      query:(data)=>({
+        url:"users/create-faculty",
+        method:"POST",
+        body:data
+      })
+    })
   }),
 });
 
